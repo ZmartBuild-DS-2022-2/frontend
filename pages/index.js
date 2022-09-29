7c87f681dace272b29281846965676e4fb9f14aa
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-09-29 11:27:30 -0300

    lint: fix

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
 import Head from 'next/head'
 
 export default function Home() {
-  console.log('one');
-  var ww = process.env.NODE_ENV.trim.toLowerCase();
-  if (ww === 'production') {
-    console.log('Another one');
-  } else {
-    console.log('test');
-  }
   return (
     <>
       <Head>
