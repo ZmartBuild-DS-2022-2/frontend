a6037d6f874d7843c3ce4d6d7e193fa52182dd2c
NicoMeyer <nmeya@uc.cl>
2022-10-29 22:10:20 -0300

    Added initial preview for GTlf file rendering

---
+++ components/Model.jsx
@@ -0,0 +1,39 @@
+import { Suspense } from "react"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Environment, OrbitControls } from "@react-three/drei"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { Html, useProgress } from "@react-three/drei"
+
+function Loader() {
+  // Show the current loading progress instead of a blank space
+  const { progress } = useProgress()
+  return <Html center>{Math.round(progress)} % loaded</Html>
+}
+
+const Model = ({ name, scale }) => {
+  // location of the 3D model,
+  // starts with http://localhost:3000/models because it requires an absolute URL
+  // name is the name of the folder where the model is saved
+  const gltf = useLoader(GLTFLoader, `http://localhost:3000/models/${name}/scene.gltf`)
+  return (
+    <>
+      {/* Use scale to control the size of the 3D model */}
+      <primitive object={gltf.scene} scale={scale} />
+    </>
+  )
+}
+
+export default function LoadModel({ name, scale, autoRotate }) {
+  return (
+    <Canvas camera={{ position: [0, 0, 4], fov: 50 }}>
+      <ambientLight intensity={1.7} />
+      <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+      <Suspense fallback={<Loader />}>
+        <Model name={name} scale={scale} />
+        {/* To add environment effect to the model */}
+        <Environment preset="city" />
+      </Suspense>
+      <OrbitControls autoRotate={autoRotate} />
+    </Canvas>
+  )
+}
