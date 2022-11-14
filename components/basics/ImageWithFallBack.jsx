66e28f07b2a9fd0621dfea259254bacb9bd07b4f
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-11-14 17:09:19 -0300

    feat(fallBackImage): added component that handles images fallback

---
+++ components/basics/ImageWithFallBack.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+import Image from "next/image"
+
+const ImageWithFallback = (props) => {
+  const fallbackSrc = "/fallbackimage.png"
+  const { src, alt, ...rest } = props
+  const [imgSrc, setImgSrc] = useState(src)
+
+  return (
+    <Image
+      {...rest}
+      alt={alt}
+      src={imgSrc}
+      onError={() => {
+        setImgSrc(fallbackSrc)
+      }}
+    />
+  )
+}
+
+export default ImageWithFallback
