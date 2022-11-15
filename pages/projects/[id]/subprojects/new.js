bd76f277111a017a9a329f5fff7b1a6205dfa9ce
FeBalla <fballadares@uc.cl>
2022-11-15 16:51:28 -0300

    fix: remove model from landing page

--- pages/projects/[id]/subprojects/new.js
+++ pages/projects/[id]/subprojects/new.js
@@ -1,12 +1,20 @@
 import Head from "next/head"
 import PageSpinner from "../../../../components/PageSpinner"
-
-import SubprojectForm from "../../../../components/subprojects/SubprojectForm"
+// import SubprojectForm from "../../../../components/subprojects/SubprojectForm"
 import Header from "../../../../components/header/Header"
 import { useUser } from "../../../../hooks/useUser"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 export default function NewProject() {
   const [isAuthenticated, isLoadingUser] = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isLoadingUser && !isAuthenticated) {
+      router.push("/login")
+    }
+  }, [router, isAuthenticated, isLoadingUser])
 
   return (
     <>
@@ -24,7 +32,7 @@ export default function NewProject() {
         <>
           <Header />
           <main>
-            <SubprojectForm />
+            {/* <SubprojectForm /> */}
           </main>
         </>
       )}
