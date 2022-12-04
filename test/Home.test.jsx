7706f5ddade7d3e384309702ee02e18337813810
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-12-04 12:02:51 -0300

    fix(test): home page test working

--- test/Home.test.jsx
+++ test/Home.test.jsx
@@ -1,9 +1,18 @@
 import { render, screen } from "@testing-library/react"
+import { useState, createContext, useEffect } from "react"
+import { AuthContextProvider } from "../stores/AuthContext"
+import "@testing-library/jest-dom"
+
 import Home from "../pages/index"
 
 test("Given render App When the page load Then show text Zmartbuild", () => {
   // Given
-  render(<Home />)
+  const context = { user: "yes" }
+  render(
+    <AuthContextProvider value={context}>
+      <Home />
+    </AuthContextProvider>
+  )
 
   // When
   const homeElement = screen.getByText(/ZmartBuild/i)
