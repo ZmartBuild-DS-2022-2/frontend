ad04118f7a0a9977501f7b9fd754ff948582fcf9
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-12-08 16:48:20 -0300

    feat(landingPage): looking good for desktop

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ import img3 from "../public/ladingPageImages/3.jpg"
 import Link from "next/link"
 import Carousel from "../components/Carousel"
 
-const GetStaterd = () => {
+const GetStaterd = ({ className }) => {
   return (
     <Link href="/register">
       <PrimaryButton
         type="button"
-        className="px-8 bg-primary text-primary-contrast hover:bg-primary-hover"
+        className={`px-8 bg-primary text-primary-contrast hover:bg-primary-hover ${className}`}
       >
         Get Started
       </PrimaryButton>
@@ -34,16 +34,16 @@ export default function Home() {
 
       <main>
         <div className="flex flex-col p-2 md:py-4 md:px-8">
-          <div className="flex flex-col md:flex-row-reverse items-center md:justify-between">
-            <div className="flex flex-col items-center p-2">
-              <p className="text-3xl font-medium">3D Model Visualizer</p>
-              <p>Upload, share and visualize</p>
-              <p>It&apos;s simple.</p>
+          <div className="flex flex-col md:flex-row-reverse items-center md:justify-end">
+            <div className="flex flex-col items-center p-2 md:w-[35vw]">
+              <p className="text-3xl md:text-4xl font-medium">3D Model Visualizer</p>
+              <p className="md:text-xl">Upload, share and visualize.</p>
+              <p className="md:text-xl">It&apos;s simple.</p>
               <div className="p-4 hidden md:block">
-                <GetStaterd />
+                <GetStaterd className="md:text-lg " />
               </div>
             </div>
-            <div className="relative w-[95vw] m-auto h-[40vh] md:w-[70vw]  ">
+            <div className="relative w-[95vw] h-[40vh] md:w-[60vw] md:h-[75vh] md:justify-self-start ">
               <Carousel images={[img1, img2, img3]} className="rounded-xl" objectFit="cover" />
             </div>
             <div className="p-4 md:hidden">
