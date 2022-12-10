d37e167598775918951e94d4fd94bd0874731f6a
Catalina Ramirez <catalina.ramrez@uc.cl>
2022-12-10 12:14:17 -0300

    delete test that dont consider the new landing page

--- test/Home.test.jsx
+++ test/Home.test.jsx
@@ -4,22 +4,6 @@ import "@testing-library/jest-dom"
 
 import Home from "../pages/index"
 
-test("Given render Home When the page load Then show text Zmartbuild", () => {
-  // Given
-  const context = { user: "yes" }
-  render(
-    <AuthContextProvider value={context}>
-      <Home />
-    </AuthContextProvider>
-  )
-
-  // When
-  const homeElement = screen.getByText(/ZmartBuild/i)
-
-  // Then
-  expect(homeElement).toBeInTheDocument()
-})
-
 test("Given render Home When the page load Then show login because there is no valid user", () => {
   // Given
   const context = { user: "yes" }
